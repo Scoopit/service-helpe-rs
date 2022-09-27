33504ba4eeed446da376a3af1fbf42e685f1c3b1
Philippe GASSMANN <philoops@gmail.com>
2022-09-27 23:08:23 +0200

    add doc

--- src/axum/options.rs
+++ src/axum/options.rs
@@ -5,11 +5,20 @@ use axum::{
 };
 use http::{Method, Request};
 
+/// Unconditionally handle `OPTIONS` requests. Send a `200 OK` response
+/// with `Allow` header set to `OPTIONS, GET, HEAD, POST, PUT, DELETE`.
+///
+/// This middleware is especially useful when a load balancer checks the availability
+/// of the service by sending an `OPTIONS` http request (eg. haproxy).
+///
 pub async fn options_middleware<B>(req: Request<B>, next: Next<B>) -> impl IntoResponse {
     if req.method() == Method::OPTIONS {
         Response::builder()
             .status(200)
-            .header(axum::http::header::ALLOW, "OPTIONS, GET, HEAD, POST")
+            .header(
+                axum::http::header::ALLOW,
+                "OPTIONS, GET, HEAD, POST, PUT, DELETE",
+            )
             .body(boxed(Body::empty()))
             .unwrap()
     } else {
